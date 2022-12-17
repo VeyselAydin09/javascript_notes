1f573585f3f2e524f9568dec8f613c1ab509688e
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-17 12:51:10 +0300

    conditions

---
+++ js_4/conditions.js
@@ -0,0 +1,32 @@
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+//**************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+// const sayi = prompt("Bir Sayi Giriniz")
+// if(sayi > 0 ) {
+// console.log(`${sayi} pozitiftir.`);}
+// else if (sayi ==0){
+//     console.log(`${sayi}'a esittir.`);
+// }else {
+//     console.log(`${sayi} negatiftir.`);
+// } console.log(sayi, typeof sayi);
+
+// 1. YONTEM
+//     const n1 = +prompt("sayi1:")
+//     const n2 = +prompt("sayi2:")
+//    const n3 = +prompt("sayi3:")
+
+//    if(n1 >= n2 && n1>=n3){
+//     console.log(`${n1} en buyuk sayidir.`);
+//    }
+
+//    else if(n2>=n1 && n2>=n3){
+//     console.log(`${n2} en buyuk sayidir`);
+//    }
+//    else if(n3>=n1 && n3>=n2){
+//     console.log(`${n3} en buyuk sayidir`);
+//    }
