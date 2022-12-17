a0f5076fda1ca7a961e8527ac884f117250b20f1
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-17 13:17:49 +0300

    conditions

--- js_4/conditions.js
+++ js_4/conditions.js
@@ -30,3 +30,114 @@ console.log("****** CONDITIONS *******");
 //    else if(n3>=n1 && n3>=n2){
 //     console.log(`${n3} en buyuk sayidir`);
 //    }
+
+// 2. YONTEM
+
+// let enBuyuk = n1;
+
+// if (n2 >= enBuyuk) {
+//     enBuyuk = n2;
+
+// }if (n3 >= enBuyuk){
+//     enBuyuk = n3;
+// }
+// console.log(`${enBuyuk} en buyuktur`);
+
+// const not = +prompt("Notunuzu Giriniz:");
+
+// console.log(not >=50 ? "Gecti" : "kaldi");
+
+// const yas = 17;
+// const cinsiyet = "erkek";
+// const saglikli = true ;
+
+// yas >=18 && cinsiyet === "erkek" && saglikli
+// ? console.log("Askerlik yapmali")
+// : console.log("Askerlik yapmasina gerek yok");
+
+//!ODEV=1 DORT ISLEM HSAP MAKINESI YAP!!!!!!!!//
+//!===SWITCH-CASE===//
+//!ODEV=2 SWITCH ILE CLARUSWAY DERS PROGRAMI HAFTALIK KULLANICIDAN SAYI TALEP EDEREK 1-7 ARASINDA//
+
+// /!====ODEV-1======////
+
+// const n1 = +prompt("sayi1:")
+// const operator = prompt("enter +,-,*,/")
+// const n2 = +prompt("sayi2:")
+
+// if (operator === "+"){console.log(n1+n2)
+// }
+
+// if (operator === "-"){console.log(n1-n2)
+// }
+
+// if (operator === "*"){console.log(n1*n2)
+// }
+
+// if (operator === "/"){console.log(n1/n2)
+// }
+
+// /!=======ODEV-2======////
+
+// let gun = prompt("gun giriniz");
+// switch (gun) {
+//   case "pazartesi":
+//     console.log("Inclass");
+//     break;
+//   case "sali":
+//     console.log("Inclass");
+//     break;
+//   case "carşamba":
+//     console.log("Inclass");
+//     break;
+//   case "perşembe":
+//     console.log("Inclass");
+//     break;
+//   case "cuma":
+//     console.log("Teawork");
+//     break;
+//   case "cumartesi":
+//     console.log("Inclass + Workshop");
+//     break;
+//   case "pazar":
+//     console.log("Self study");
+//     break;
+//   default:
+//     console.log("Yanlış gün girildi");
+// }
+// console.log(gun);
+
+// /!=========ODEV-3=========/////
+
+// const maas = +prompt("maas giriniz");
+// if (maas <= 5500) {
+//   console.log(maas * 1.5);
+// }
+// if (maas >= 5500) {
+//   console.log(maas * 1.1);
+// }
+
+// /!=====ODEV-4=======////
+
+// let gelir = +prompt("gelir miktarini giriniz");
+// let gider = +prompt("gider miktarini giriniz");
+// let sonuc = (gelir>=gider+5500) ? "kredi verilebilir" : "kredi verilemez"
+// console.log(`${sonuc}`)
+
+// const gelir = +prompt("gelir miktarini giriniz");
+// const gider = +prompt("gider miktarini giriniz");
+
+// if (gelir>=gider+5500){
+//     console.log("kredi verilebilir")
+// }
+// if (gelir<gider+5500){
+//     console.log("kredi Verilemez")
+// }
+
+// let sum = 0;
+// let number = 0;
+// do {
+//   sum += number;
+//   number = parseInt(prompt("Enter a number: "));
+// } while (number >= 0);
+// alert(`The sum is ${sum}.`);
