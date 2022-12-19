d44414dae961c6695613f6f3e9972dee6934fc64
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-19 16:20:31 +0300

    functionDeclaration finished

--- js_6/1-functionDeclaration.js
+++ js_6/1-functionDeclaration.js
@@ -19,3 +19,42 @@ function yazdir() {
 
 // yazdir(); //! invoke, call, cagirma
 // yazdir();
+//* ORNEK2:
+//*************************************************/
+
+// function selamla(ad, soyAd = "") {
+//   console.log(`Merhaba ${ad} ${soyAd}`);
+// }
+
+// selamla("Can", "Yilmaz");
+// selamla("Canan", "Ozturk");
+// selamla("Ayse");
+// selamla("John");
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+
+//* ORNEK3:
+//***************************************************/
+
+// function yasHesapla(isim, dogumTarihi) {
+//   const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir.`;
+//   const sonuc = `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+//   return sonuc;
+//   return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+// }
+
+// const mesaj1 = yasHesapla("Elif Can", 1990);
+// console.log(mesaj1);
+// console.log(yasHesapla("Veli Canan", 1980));
+
+//* ORNEK4:
+//*****************************************************/
+
+// function tekCift(sayi) {
+//   return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+// }
+
+// console.log(tekCift(5));
+// console.log(tekCift(2));
