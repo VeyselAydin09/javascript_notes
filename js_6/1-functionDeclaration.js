76a8c255361e9d0fed127510544c98e55906a7d1
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-19 16:12:30 +0300

    functionDeclaration

---
+++ js_6/1-functionDeclaration.js
@@ -0,0 +1,21 @@
+// ? ===============================================
+// ?               FONKSİYONLAR
+// ? ===============================================
+
+// !------------------------------------------------
+// ! 1.YONTEM  : FUNCTION DECLARATION
+// !------------------------------------------------
+
+console.log("**** FUNC DECLARATION ******");
+
+//* ORNEK:
+//************************************************/
+yazdir();
+
+//! Fonksiyonun tanimlanmasi (declaration)
+function yazdir() {
+  console.log("merhaba");
+}
+
+// yazdir(); //! invoke, call, cagirma
+// yazdir();
