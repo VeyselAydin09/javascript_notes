b4b6d6be298d735bba7deda72324694f679ea99d
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-21 21:21:47 +0300

    js

---
+++ js_8/stringMethods.js
@@ -0,0 +1,22 @@
+// // ?======================================================
+// // ?                     STRING METOTLARI
+// // ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// // ?======================================================
+
+// console.log("*** STRING METHODS ***");
+
+// //!1- Klasik yöntem (String Literals) ile string tanimlanirsa bu string
+// //! bir primitive degiskendir.
+// let str1 = "Clarusway";
+// //Char => C, l, a,....
+// const str2 = " Hello";
+// const str3 = " Full Stack";
+
+// console.log(str1, typeof str1);
+
+// //!2 - Non-primitive String tanımlama
+// const str4 = new String("Non-Primitive");
+// console.log(typeof str4);
+
+// //?----------------------------------------
+// console.log(str1.toLowerCase());
