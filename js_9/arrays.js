6449b6d99ea14548c925ed5e7929a13bd750f4a7
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-22 20:07:50 +0300

    methods

--- js_9/arrays.js
+++ js_9/arrays.js
@@ -15,3 +15,23 @@ console.log(isimler.length);
 
 const bosDizi = [];
 console.log(bosDizi);
+// ! 2.Yöntem (Object Constructor)
+const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
+console.log(diller);
+
+const numbers = new Array(3, 2, 1);
+console.log(numbers);
+
+//! 10 elemanlik bos bir Array oluşturdu
+const numbers1 = new Array(10);
+console.log(numbers1);
+
+// ! 3.Yöntem (Array.of())
+const veriler = Array.of(1, 2, 3);
+console.log(veriler);
+
+const veri = Array.of("Deneme");
+console.log(veri);
+
+//* Diziden Veri Okuma-Yazma (indisleme)
+//* ---------------------------------------------------------
