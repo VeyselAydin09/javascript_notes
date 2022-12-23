a29ddd10fa37cd1039440772a4e1a16f74963be6
Veysel Aydin <weycell.aydin09@gmail.com>
2022-12-23 17:33:29 +0300

    loops

---
+++ js_10/1-loops.js
@@ -0,0 +1,38 @@
+//* =====================================================
+//*                     FOR LOOP
+//* ======================================================
+
+console.log("******* LOOPS IN ARRAYS ******");
+
+//?-------------- ÖRNEK -------------------
+// const dizi = [-5, 15, 22, -4, 45, 78];
+
+// const negatifler = [];
+// const pozitifler = [];
+
+// const dizelereAyir = (n) => {
+//   for (let i = 0; i < n.length; i++) {
+//     if (n[i] < 0) {
+//       negatifler.push(n[i]);
+//     } else {
+//       pozitifler.push(n[i]);
+//     }
+//   }
+// };
+// dizelereAyir(dizi);
+
+// console.log(negatifler);
+// console.log(pozitifler);
+// console.log(dizi);
+
+//?-------------- ÖRNEK -------------------
+//? Dizideki notlarin ortalamasini hesaplayiniz.
+// const notlar = [55, 77, 23, 89, 100, 98];
+
+// let toplam = 0;
+
+// for (let i = 0; i < notlar.length; i++) {
+//   toplam += notlar[i];
+// }
+
+// console.log("ORTALAMA:", toplam / notlar.length);
