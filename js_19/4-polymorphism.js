182ad532238c0477b7b8d6f02e724f352ef0e40b
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-02 22:43:16 +0300

    polymorphism.js

---
+++ js_19/4-polymorphism.js
@@ -0,0 +1,34 @@
+//* ======================================================
+//*        OOP -  Polymorphism(ES6)
+//* ======================================================
+
+//* Polymorphism, bir degisken, fonksiyon veya nesnenin çoklu sekiller
+//* alabilmesini tanimlayan bir nesne-yonelimli programlama teknigidir.
+//* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar
+//* ile bilinir.
+
+class Book {
+  constructor(title, author, year) {
+    this.author = author;
+    this.title = title;
+    this.year = year;
+
+    //? Bu alanda yazilan bir metot butun instance'ların belleginde tek tek yer kaplar.
+    this.getTitle = function () {
+      return this.title;
+    };
+  }
+
+  //! Bu kisimda yazilan fonksiyonlar aslinda prototype alaninda bulunur.
+  getAge() {
+    return new Date().getFullYear() - this.year;
+  }
+  getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year}`;
+  }
+
+  setPrice(price) {
+    const taxRate = 1.1;
+    this.price = Math.trunc(price * taxRate);
+  }
+}
