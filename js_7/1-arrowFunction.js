13ddb0f7d606463d10c5ac91540a33a363e1fada
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-05 18:27:31 +0300

    repeated

--- js_7/1-arrowFunction.js
+++ js_7/1-arrowFunction.js
@@ -10,15 +10,12 @@
 
 // //* ORNEK: Silindir Hacmi
 // //************************************************/
+const r = +prompt("lutfen bi sayi giriniz");
+const h = +prompt("lutfen bi sayi giriniz");
 
-// const r = Number(prompt("yaricap giriniz:"));
-// const h = Number(prompt("yukseklik giriniz:"));
+const hacimHesapla = (r, h) => Math.PI * r * r * h;
 
-// const hacimHesapla = (r, h) => Math.PI * r * r * h;
-
-// // console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
-// console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
-
+console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
 // //* ORNEK: Yas hesapla
 // //************************************************/
 
@@ -62,27 +59,27 @@
 //* hesaplayarak yazdiran fonksiyonu dongu ile kodlayiniz.
 //? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
 
-let stringResult = "1,1";
-// f(n) = f(n-1) + f(n-2)
-const fibo = (n) => {
-  let fib1 = 1;
-  let fib2 = 1;
-  let toplam = 0;
+// let stringResult = "1,1";
+// // f(n) = f(n-1) + f(n-2)
+// const fibo = (n) => {
+//   let fib1 = 1;
+//   let fib2 = 1;
+//   let toplam = 0;
 
-  for (let i = 2; i < n; i++) {
-    toplam = fib1 + fib2;
-    fib1 = fib2;
-    fib2 = toplam;
-    stringResult += "," + fib2;
-  }
+//   for (let i = 2; i < n; i++) {
+//     toplam = fib1 + fib2;
+//     fib1 = fib2;
+//     fib2 = toplam;
+//     stringResult += "," + fib2;
+//   }
 
-  return fib2;
-};
+//   return fib2;
+// };
 
-const n = +prompt("n terimini giriniz:");
-if (n <= 0) {
-  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
-} else {
-  console.log(`FIBO(${n})= ${fibo(n)}`);
-  console.log(stringResult);
-}
+// const n = +prompt("n terimini giriniz:");
+// if (n <= 0) {
+//   console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
+// } else {
+//   console.log(`FIBO(${n})= ${fibo(n)}`);
+//   console.log(stringResult);
+// }
