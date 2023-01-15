1c70ae8877abab73ced647e13e5f0ee803589d39
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-15 13:01:48 +0300

    question3

--- algorithm_examples/script.js
+++ algorithm_examples/script.js
@@ -46,3 +46,13 @@ if (3 <= elaAge) {
 }
 
 /--------------------------------question-3----------------------------/;
+
+let prices = [1, 2, 3, 4, 5, 6];
+
+let profit = 0;
+
+for (let i = 1; i < prices.length; i++) {
+  prices[i] > prices[i - 1];
+  profit += prices[i] - prices[i - 1];
+}
+console.log(profit);
